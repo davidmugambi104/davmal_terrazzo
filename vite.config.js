018b6095096f9ec1255411dc65faa5ed4bbb4777
david-mugambi <davidmugambi104@gmail.com>
2025-04-13 12:00:57 +0300

    addds

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'automatic',
+      jsxRuntime: 'automatic', // Add this line
       include: /\.(tsx|ts|jsx|js)$/,
-    })
+    }),
   ],
-  base: './',
-  build: {
-    outDir: 'dist',
-    sourcemap: 'hidden',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          firebase: ['firebase/app', 'firebase/auth']
-        }
-      }
-    }
-  },
   esbuild: {
     loader: 'tsx',
     include: /src\/.*\.(tsx|ts|jsx|js)$/,
-  }
+  },
 });
+
